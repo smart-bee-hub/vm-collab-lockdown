0e7a4cbaf8ff8e558c87abfe3260a06d2f972cf7
smart-bee-hub <150753321+smart-bee-hub@users.noreply.github.com>
2026-03-04 02:26:29 +0100

    Revise notifications about GUI settings permissions
    Updated user notification messages to clarify permission changes for GUI settings.

--- Lockdown.sh
+++ Lockdown.sh
@@ -24,10 +24,8 @@ chmod 000 /usr/bin/xfce4-session-logout
 cat << 'EOF' >> /home/dartz/.bashrc
 
 echo "A user named beezled ran a script to stop people from creating chaos through power and display settings."
-echo "Using the GUI display settings and the session log out apps will not be possible unless you run the binaries as root"
-echo "After having changed their permissions to 700 (something else if you want non root users to be able to have full"
-echo "Access to them)"
-echo "which are located at /usr/bin/xfce4-display-settings and /usr/bin/xfce4-session-logout."
+echo "Using the GUI display settings and the session log out apps will not be usable until you change their permissions"
+echo "By running sudo chmod 755 /usr/bin/xfce4-display-settings and /usr/bin/xfce4-session-logout "
 echo "Rebooting and changing the display settings through the terminal are unaffected. Contact me on Discord"
 echo "@beezled for any questions."
 echo "Information on the script available at"
